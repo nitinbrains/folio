38323b8ed8d61a8a22b94b12a6832ad9cf8bc72c
nitinbrains <nitinbrains@gmail.com>
2021-01-20 00:04:29 +0530

    f4

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,13 +22,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Simplefolio`,
-        short_name: `Simplefolio`,
+        name: `nitpedia`,
+        short_name: `NitinSharma`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#02aab0`,
         display: `standalone`,
-        icon: 'src/images/favicon.png',
+        icon: 'src/images/favicon.svg',
       },
     },
   ],
