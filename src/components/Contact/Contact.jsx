bec18c6c32202c94335691eea23848edffceea50
nitinbrains <nitinbrains@gmail.com>
2021-01-19 23:47:41 +0530

    f

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -15,15 +15,15 @@ const Contact = () => {
         <Fade bottom duration={1000} delay={800} distance="30px">
           <div className="contact-wrapper">
             <p className="contact-wrapper__text">
-              {cta || 'Would you like to work with me? Awesome!'}
+             'Would you like to work with me? Awesome! Write to me @ <span style={{fontSize:'25px',fontWeight:'bolder'}}>nitinbrains@gmail.com</span> 
             </p>
             <a
               target="_blank"
               rel="noopener noreferrer"
               className="cta-btn cta-btn--resume"
-              href={email ? `mailto:${email}` : 'https://github.com/cobidev/react-simplefolio'}
+              href={email ? `mailto:${email}` : 'nitinbrains@gmail.com'}
             >
-              {btn || "Let's Talk"}
+              {btn || "Coffee with me"}
             </a>
           </div>
         </Fade>
